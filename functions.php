8e07921dda98bb963dd1985ae591406b16112e43
hccadmin <dave_rothfarb@dfci.harvard.edu>
2018-05-21 11:01:39 -0400

    Removing Hale Center references

--- functions.php
+++ functions.php
@@ -3,12 +3,12 @@ function nirvana_halecenter_enqueue_styles() {
 
   $parent_style = 'nirvanas';
   wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-  wp_enqueue_style('nirvana_halecenter',
+  wp_enqueue_style('nirvana_child',
     get_stylesheet_directory_uri() . '/style.css',
     array($parent_style), 
     wp_get_theme()->get('Version')
   );
 }
 
-add_action('wp_enqueue_scripts', 'nirvana_halecenter_enqueue_styles');
+add_action('wp_enqueue_scripts', 'nirvana_child_enqueue_styles');
 ?>
