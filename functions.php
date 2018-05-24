db394b66fbd0594b40cbf0c30f8367cf68be9f0e
hccadmin <dave_rothfarb@dfci.harvard.edu>
2018-05-24 13:50:32 -0400

    Added hero and footer widget functions to functions.php

--- functions.php
+++ functions.php
@@ -11,4 +11,28 @@ function nirvana_child_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'nirvana_child_enqueue_styles');
-?>
+
+function hero_widgets_init() {
+  register_sidebar( array(
+    'name' => 'Hero area',
+    'id' => 'hero-image-widget',
+    'before_widget' => '<div id="%1$s" class="hero-iamge">',
+    'after_widget' => '</div>',
+    'before_title' => '<h2>',
+    'after_title' => '</h2>',
+  ) );
+}
+
+add_action('widgets_init', 'hero_widgets_init');
+
+function footer_bottom_widgets_init() {
+  register_sidebar( array(
+    'name' => 'Footer bottom',
+    'id' => 'footer-bottom-widget',
+    'before_widget' => '<div id="%1$s" class="footer-bottom">',
+    'after_widget' => '</div>',
+  ) );
+}
+
+
+add_action('widgets_init', 'footer_bottom_widgets_init');
