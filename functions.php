6a231d02075045a06653f0ddbc29533501a8a2ab
hccadmin <dave_rothfarb@dfci.harvard.edu>
2018-10-11 13:48:13 -0400

    Added more directories

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function nirvana_child_enqueue_styles() {
   $parent_style = 'nirvanas';
   wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
   wp_enqueue_style('nirvana_child',
-    get_stylesheet_directory_uri() . '/style.css',
+    get_stylesheet_directory_uri() . '/css/style.css',
     array($parent_style), 
     wp_get_theme()->get('Version')
   );
